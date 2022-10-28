60c1aea6c9fb1afc91451a9bee3d7c32ac24f32f
Anand S <anandzzz360@gmail.com>
2022-10-28 22:45:11 +0530

    include: update included headers

--- include.h
+++ include.h
@@ -1,18 +1,22 @@
 #ifndef INCLUDE_H
 #define INCLUDE_H
 
-  #include <RadioLib.h>
-  // #include <HttpClient.h> 
-  // #include <WiFi.h> 
-  // #include <DFRobot_LIS2DW12.h>
+  #include <RadioLib.h> 
   // #include <FastLED.h>
-
-  #include "http/http.h"
+  #include <ESP8266WiFi.h>
+  #include <WiFiClientSecure.h> 
+  #include <ESP8266WebServer.h>
+  #include <ESP8266HTTPClient.h>
+  
+  // #include "ESP8266WiFi/SerialESP8266wifi.h"
+  // #include "ESP8266HttpClient/ESP8266HttpClient.h"
+  
+  
   #include "pins.h"
   #include "globals.h"
   // #include "Morse/led.h"
   #include "LoRa/LoRa.h"  
+  // #include "http/http.h"
 
-  // #include "DFRobot/LIS2DW12.h"
   
 #endif
