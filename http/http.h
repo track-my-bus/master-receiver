5c01a431a348ecb158ad51f0666aaa7836f0893d
Anand S <anandzzz360@gmail.com>
2022-10-28 22:59:04 +0530

    http: refresh http.h with new credentials

--- http/http.h
+++ http/http.h
@@ -1,9 +1,10 @@
 #ifndef HTTP_H
 #define HTTP_H 
-    
-    char ssid[]="Twilighty's S20+";
-    char pass[]="gear1008";
-    char url[] = "http://localhost:3000/data";
+    const char *ssid = "GL7";  //ENTER YOUR WIFI SETTINGS
+    const char *password = "";
+    const char *host = "trackmybus-api.herokuapp.com";
+    const int httpsPort = 443;  //HTTPS= 443 and HTTP = 80
 
+    const char fingerprint[] PROGMEM = "D6 27 27 66 FB 8A 91 E6 31 3A B8 AF B1 79 4B 8F C7 A6 5A DC";
     #include "http.c"
 #endif
